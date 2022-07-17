0a5defc8a878b29dc962580939bc0afbc3e641be
xinchengforgit <2419743144@qq.com>
2022-07-17 18:00:25 +0800

    finish the project1

--- courses/rust/projects/project-1/src/lib.rs
+++ courses/rust/projects/project-1/src/lib.rs
@@ -1,5 +1,4 @@
-use core::panic;
-
+use std::collections::HashMap;
 //lib.rs是相当于package的root crate
 #[warn(non_snake_case)]
 // pub mod KvStore {
@@ -16,23 +15,27 @@ use core::panic;
 //         panic!("remove unimplemented");
 //     }
 // }
-pub struct KvStore {}
+pub struct KvStore {
+    mp: HashMap<String, String>,
+}
 
 impl KvStore {
     // 注意Self大小写不一样是有区别的，小写的self是相当于一个crate
     pub fn new() -> Self {
-        KvStore {}
+        KvStore { mp: HashMap::new() }
     }
     // 神必的 self 和 Self
     pub fn set(&mut self, key: String, val: String) {
-        panic!("{}, {}", key, val);
+        self.mp.insert(key, val);
     }
 
+    // 关于Option<&T> to Option<T>的方法, map 是一种, 它可以把Option<U> -> Option<T>
     pub fn get(&self, key: String) -> Option<String> {
-        panic!("{}", key);
+        self.mp.get(&key).cloned() // 注意cloned的方法, 专门用来将Option<&T> -> Option<T>
+                                   // self.mp.get(&key).map(|x| x.to_owned())
     }
 
-    pub fn remove(&self, key: String) {
-        panic!("{}", key);
+    pub fn remove(&mut self, key: String) {
+        self.mp.remove(&key);
     }
 }
